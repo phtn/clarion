35dc63acbe50329b2eca574e6ad6f2e5b94dfbb2
phtn <phtn458@gmail.com>
2017-10-23 12:53:00 -0400

    resized images

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Grid, Image, Message, Icon } from 'semantic-ui-react'
+import { Grid, Image, Message, Icon, Divider } from 'semantic-ui-react'
 import NH1 from '../assets/nh1.jpg'
 import NH2 from '../assets/nh2.jpg'
 import NH3 from '../assets/nh3.jpg'
 import NH4 from '../assets/nh4.jpg'
+import PV1 from '../assets/pv1.jpg'
+
 export default props => (
   <div>
     <Message
@@ -15,6 +17,27 @@ export default props => (
     <Grid padded>
       <Grid.Column width={4}>
         <Image src={NH1} shape={'rounded'}/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <Image src={NH2} shape={'rounded'}/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <Image src={NH3} shape={'rounded'}/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <Image src={NH4} shape={'rounded'}/>
+      </Grid.Column>
+    </Grid>
+    <Divider/>
+    <Message
+      icon='street view'
+      header="Peddler's Village"
+      content='Shops | Restaurants | Pubs '
+      success
+    />
+    <Grid padded>
+      <Grid.Column width={4}>
+        <Image src={PV1} shape={'rounded'}/>
       </Grid.Column>
       <Grid.Column width={4}>
         <Image src={NH2} shape={'rounded'}/>
